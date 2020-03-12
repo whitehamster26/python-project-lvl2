ca9ffc55cb869474d72c99dc427b7acaf8d1a954
DSemenko <me@dsemenko.ru>
2020-03-12 18:48:21 +0300

    Connected argparse

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,15 @@
+import argparse
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file', type=str, help='select first file \
+     to compare')
+parser.add_argument('second_file', type=str, help='select second file \
+     to compare')
+parser.add_argument('-f', '--format', type=str, help='set format of \
+    output')
+
+
 def main():
-    print("Hello world!")
+    args = parser.parse_args()  # noqa: F841
 
 
 if __name__ == "__main__":
