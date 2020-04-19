0ba99139d260a0fd894d0d63e6f6b00ae5d29f09
WhiteHamster <whitehamster26@gmail.com>
2020-04-19 09:27:31 +0300

    Modified parsers module

--- gendiff/parsers.py
+++ gendiff/parsers.py
@@ -2,17 +2,20 @@ import json
 import yaml
 import os
 
+SUPPORTED_FORMATS = (JSON, YAML, YML) = ('.json', '.yaml', '.yml')
+
 
 def get_data(source_file):
     file_format = os.path.splitext(source_file)[1]
-    return _parse_data(file_format, source_file)
+    if file_format not in SUPPORTED_FORMATS:
+        raise Exception('This file format is not supported.')
+    else:
+        return _parse_data(file_format, source_file)
 
 
 def _parse_data(format, source_file):
     with open(source_file, 'r') as f:
-        if format == '.json':
+        if format == JSON:
             return json.load(f)
-        elif format in ('.yaml', '.yml'):
+        elif format in (YAML, YML):
             return yaml.safe_load(f)
-        else:
-            raise Exception('This file format is not supported.')
